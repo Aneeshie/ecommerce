358bdd180fe26908d7ac0f594e1ed4d687e6fb2c
Aneeshie <aneeshdas556@gmail.com>
2026-07-06 20:29:05 +0530

    feat(identity): add refresh token persistence

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -6,6 +6,6 @@ CREATE TABLE users (
     password_hash TEXT NOT NULL,
     role TEXT NOT NULL,
     email_verified BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMP NOT NULL,    
+    created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 )
