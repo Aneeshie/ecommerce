217664228f0a087a92f116d6bc6d04651fa1f1c3
Aneeshie <aneeshdas556@gmail.com>
2026-07-14 19:27:09 +0530

    feat(store): add repositories to store

---
+++ internal/store/store.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	identityRepository "github.com/Aneeshie/ecommerce/internal/identity/repository"
+	inventoryRepository "github.com/Aneeshie/ecommerce/internal/inventory/repository"
+	productRepository "github.com/Aneeshie/ecommerce/internal/product/repository"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Store struct {
+	pool *pgxpool.Pool
+}
+
+func NewStore(pool *pgxpool.Pool) *Store {
+	return &Store{
+		pool: pool,
+	}
+}
+
+func (s *Store) Products() *productRepository.Repository {
+	return productRepository.NewRepository(s.pool)
+}
+
+func (s *Store) Inventory() *inventoryRepository.Repository {
+	return inventoryRepository.NewRepository(s.pool)
+}
+
+func (s *Store) Users() *identityRepository.Repository {
+	return identityRepository.NewRepository(s.pool)
+}
