55b4db63669953abc1f6f2b574b1cff69a805652
Aneeshie <aneeshdas556@gmail.com>
2026-07-21 14:44:38 +0530

    feat: init shadcn

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,13 @@
+import { LoginForm } from "@/components/auth/login-form";
+
+export default function LoginPage() {
+
+  return (
+    <div>
+      This is a login page.
+      <div>
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
