9b461e8a14406edab8a2831396e07774645f38cd
Aneeshie <aneeshdas556@gmail.com>
2026-07-21 17:00:05 +0530

    feat: add auth store

---
+++ frontend/stores/auth-store.ts
@@ -0,0 +1,20 @@
+import { User } from "@/types/user";
+import { create } from "zustand";
+
+type UserStore = {
+  user: User | null;
+  loading: boolean
+
+  setUser: (user: User) => void
+  clearUser: () => void
+  setLoading: (isLoading: boolean) => void
+}
+
+const useUserStore = create<UserStore>((set) => ({
+  user: null,
+  loading: true,
+
+  setUser: (user) => set({ user }),
+  clearUser: () => set( {user: null}),
+  setLoading: (isLoading: boolean) => set({loading: isLoading})
+}))
